dee8ed087332552000c6dc3b2635bd749a6187d2
CMPT300_HW2 <poohmetapple@gmail.com>
2018-11-04 18:03:41 -0800

    Added Unit Test target and .gitignore file

---
+++ Pic-A-DayUnitTests/Pic_A_DayUnitTests.swift
@@ -0,0 +1,15 @@
+//
+//  Pic_A_DayUnitTests.swift
+//  Pic-A-DayUnitTests
+//
+//  Created by Roy Zhao on 2018-11-04.
+//  Copyright © 2018 Takudzwa Mhonde. All rights reserved.
+//
+
+import XCTest
+
+class Pic_A_DayUnitTests: XCTestCase {
+
+    
+
+}
